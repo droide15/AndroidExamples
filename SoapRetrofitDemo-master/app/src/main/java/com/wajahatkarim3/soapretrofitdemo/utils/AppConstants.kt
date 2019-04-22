8157d042ad2ca9840f47a1a2c9e915ea49a5f76c
droide15 <nuniez.jorge@gmail.com>
2019-04-22 09:55:53 -0700

    removed

--- SoapRetrofitDemo-master/app/src/main/java/com/wajahatkarim3/soapretrofitdemo/utils/AppConstants.kt
+++
@@ -1,9 +0,0 @@
-package com.wajahatkarim3.soapretrofitdemo.utils
-
-object AppConstants {
-
-    object HTTP
-    {
-        val BASE_URL = "http://www.holidaywebservice.com/HolidayService_v2/HolidayService2.asmx/"
-    }
-}
